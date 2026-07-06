97cd3a8969ee4b6e671a4c9d9ebb49609a8f4cfe
elader73-eng <elader73@gmail.com>
2026-07-06 20:53:48 +0300

    Delete puppeteerrc.cjs

--- puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // זה יכריח את הדפדפן להישמר בתוך תיקיית הפרויקט שלא נמחקת
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
