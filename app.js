632f44982201f2917d4f442852696fde8ac8ddb4
elader73-eng <elader73@gmail.com>
2026-07-09 00:15:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const whatsappClient = new Client({
     puppeteer: {
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-    },
+    }
+});
+
     webVersionCache: {
         type: 'remote',
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
